591dcc9b4034a8eec54f52a08f02db014ca0e9b7
Juel B <juel.braganza97@gmail.com>
2021-04-11 22:48:46 +0100

    add home & header section

---
+++ js/script.js
@@ -0,0 +1,76 @@
+// display menu
+const toggleMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId)
+  const nav = document.getElementById(navId)
+
+  if (toggle && nav) {
+    toggle.addEventListener('click', () => {
+      nav.classList.toggle('show-menu')
+    })
+  }
+}
+
+toggleMenu('nav-toggle', 'nav-menu')
+
+// remove mobile menu
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+// active link for scroll
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+  const scrollY = window.pageYOffset
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight
+    const sectionTop = current.offsetTop - 50
+    sectionId = current.getAttribute('id')
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelectorAll('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+    } else {
+      document.querySelectorAll('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+
+    }
+  })
+}
+window.addEventListener('scroll', scrollActive)
+
+// change background header
+function scrollHeader() {
+  const header = document.getElementById('header')
+
+  if(this.scrollY >= 200 ) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+// show scroll top button
+function scrollTop() {
+  const scrollTop = document.getElementById('scroll-top')
+
+  if(this.scrollY >= 560 ) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
+
+// accordion
+
+const items = document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute('aria-expanded');
+
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute('aria-expanded', 'false');
+  }
+
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
